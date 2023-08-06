bfa2c489a8ae5b228690ab682f4a46b0c0016036
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2023-08-06 15:07:06 +0700

    Rearrange method

--- src/Dataset.php
+++ src/Dataset.php
@@ -12,6 +12,28 @@ class Dataset
     protected $rows = [];
 
     /**
+     * Handle dynamic method calls into the model.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     */
+    public function __call($method, $parameters): Collection
+    {
+        return Collection::make((new static)->getRows());
+    }
+
+    /**
+     * Handle dynamic static method calls into the model.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     */
+    public static function __callStatic($method, $parameters): Collection
+    {
+        return (new static)->$method(...$parameters);
+    }
+
+    /**
      * Check the class using the given trait.
      */
     protected function hasTrait(string $traitName): bool
@@ -52,27 +74,5 @@ class Dataset
     public static function header(): Collection
     {
         return Collection::make((new static)->getHeader());
-    }
-
-    /**
-     * Handle dynamic method calls into the model.
-     *
-     * @param  string  $method
-     * @param  array  $parameters
-     */
-    public function __call($method, $parameters): Collection
-    {
-        return Collection::make((new static)->getRows());
-    }
-
-    /**
-     * Handle dynamic static method calls into the model.
-     *
-     * @param  string  $method
-     * @param  array  $parameters
-     */
-    public static function __callStatic($method, $parameters): Collection
-    {
-        return (new static)->$method(...$parameters);
     }
 }
