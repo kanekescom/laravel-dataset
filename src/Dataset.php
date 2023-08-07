70ee2e1f48b394d70d3bf9341b42f7ca60ec581a
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2023-08-08 04:46:04 +0700

    Add exception and refactor to construct class

--- src/Dataset.php
+++ src/Dataset.php
@@ -10,10 +10,27 @@ class Dataset
     const UPDATED_AT = 'updated_at';
     const DELETED_AT = 'deleted_at';
 
+    protected $collection;
+
+
     /**
      * Hardcoded array rows.
      */
     protected $rows = [];
+
+    public function __construct()
+    {
+        $this->collection = Collection::make($this->getRows())
+            ->map(function ($item) {
+                return $this->transform($item);
+            });
+
+        if ($this->hasTrait('Kanekescom\Dataset\TimestampsResolver')) {
+            $this->collection = $this->collection->map(function ($item) {
+                return $this->timestampsResolver($item);
+            });
+        }
+    }
 
     /**
      * Handle dynamic method calls into the model.
@@ -21,19 +38,13 @@ class Dataset
      * @param  string  $method
      * @param  array  $parameters
      */
-    public function __call($method, $parameters): Collection
+    public function __call($method, $parameters)
     {
-        $collection = Collection::make((new static)->getRows());
-
-        if ($this->hasTrait('Kanekescom\Dataset\TimestampsResolver')) {
-            $collection = $collection->map(function ($item) {
-                return $this->timestampsResolver($item);
-            });
+        if (method_exists($this->collection, $method)) {
+            return call_user_func_array([$this->collection, $method], $parameters);
         }
 
-        return $collection->map(function ($item) {
-            return $this->transform($item);
-        });
+        throw new \BadMethodCallException("Method {$method} does not exist.");
     }
 
     /**
@@ -42,9 +53,13 @@ class Dataset
      * @param  string  $method
      * @param  array  $parameters
      */
-    public static function __callStatic($method, $parameters): Collection
+    public static function __callStatic($method, $parameters)
     {
-        return (new static)->$method(...$parameters);
+        if (method_exists((new static)->collection, $method)) {
+            return call_user_func_array([(new static)->collection, $method], $parameters);
+        }
+
+        throw new \BadMethodCallException("Method {$method} does not exist.");
     }
 
     /**
